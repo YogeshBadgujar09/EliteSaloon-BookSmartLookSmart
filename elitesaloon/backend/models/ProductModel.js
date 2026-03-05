5680a5eb6d41b0db6dd0658ac1e2a357b326b324
YogeshBadgujar09 <yogeshsb18@gmail.com>
2026-03-05 15:08:04 +0530

    [MODIFIED] : Yogesh - Perform CRUD operation for Product and Services

---
+++ elitesaloon/backend/models/ProductModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+
+    productName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    productType:{
+        type:String,
+        enum:["HAIRGEL","FACEWASH","SUNSCREAM"],
+        required:true
+    },
+
+    productDescription: {
+        type: String,
+        required: true
+    },
+
+    productPrice: {
+        type: Number,
+        required: true
+    },
+
+    productPreferredGender: {
+        type: String,
+        enum: ["MALE", "FEMALE", "BOTH"],
+        default: "BOTH"
+    },
+
+    productImages: [{
+        type: String
+    }],
+
+    createdDate: {
+        type: Date,
+        default: Date.now
+    }
+
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
---
+++ elitesaloon/backend/models/ServiceModel.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const ServiceSchema = new mongoose.Schema({
+
+    serviceName:{
+        type:String,
+        required:true,
+        trim:true
+    },
+
+    serviceDescription:{
+        type:String,
+        required:true
+    },
+
+    serviceType:{
+        type:String,
+        enum:["HAIRCUT","BEARD","FACIAL","MAKEUP","SKIN"],
+        required:true
+    },
+
+    serviceImages:[
+        {
+            type:String,
+            required:true
+        }
+    ],
+
+    servicePreferredGender:{
+        type:String,
+        enum:["MALE","FEMALE","BOTH"],
+        required:true
+    },
+
+    servicePrice:{
+        type:Number,
+        required:true
+    },
+
+    serviceDuration:{
+        type:Number,   // duration in minutes
+        required:true
+    },
+
+    serviceCreatedDate:{
+        type:Date,
+        default:Date.now
+    }
+
+});
+
+module.exports = mongoose.model("Service",ServiceSchema);
